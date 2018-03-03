b7ac2681443a20eb02386c218b4b62d322e488ce
priyappillai <pppillai@mit.edu>
2018-03-02 21:47:42 -0500

    Start of login

--- sigma/sigma/urls.py
+++ sigma/sigma/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
