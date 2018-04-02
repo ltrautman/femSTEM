9c10c4ba755d4eabf6c1d79aa112530361cb3b3c
priyappillai <pppillai@mit.edu>
2018-04-01 22:04:48 -0400

    Links working when run on server, more on login and register

--- sigma/sigmaweb/urls.py
+++ sigma/sigmaweb/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'aboutus/', views.aboutus, name='aboutus'),
+    url(r'dashboard/', views.dashboard, name='dashboard'),
+    url(r'faq/', views.faq, name='faq'),
+    url(r'login/', views.login, name='login'),
+    url(r'register/', views.register, name='register'),
+    url(r'scheduler/', views.scheduler, name='scheduler'),
+    url(r'timeline/', views.timeline, name='timeline'),
 ]
--- sigma/sigmaweb/views.py
+++ sigma/sigmaweb/views.py
@@ -2,3 +2,38 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {})
+
+def aboutus(request):
+    return render(request, 'aboutus.html', {})
+
+def dashboard(request):
+    return render(request, 'dashboard.html', {})
+
+def faq(request):
+    return render(request, 'faq.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+def logout(request):
+    return render(request, 'logout.html', {})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {})
+
+def scheduler(request):
+    return render(request, 'scheduler.html', {})
+
+def timeline(request):
+    return render(request, 'timeline.html', {})
