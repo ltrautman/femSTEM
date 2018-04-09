316ac5d8853225b1ec0e279b2ba38fa24d2f21dd
Miki Hansen <mikihansen0@gmail.com>
2018-04-08 22:08:40 -0400

    edits to scheduler

--- sigma/sigmaweb/templates/scheduler.js
+++ sigma/sigmaweb/templates/scheduler.js
@@ -13,8 +13,26 @@ function drop(ev) {
 };
 
 function fillTime(){
-	var start = document.getElementById("start_time");
-	var end = document.getElementById("end_time");
-	document.getElementById("start_timeslot").innerHTML = start.options[start.selectedIndex].text;
-	document.getElementById("end_timeslot").innerHTML = end.options[end.selectedIndex].text;
+	var table = document.getElementById("table");
+	var startDropdown = document.getElementById("start_time");
+	var endDropdown = document.getElementById("end_time");
+	var start = startDropdown.options[startDropdown.selectedIndex].text;
+	var end = endDropdown.options[endDropdown.selectedIndex].text;
+	startstr = start.split(":");
+	endstr = end.split(":");
+	var starttime = parseInt(startstr[0]);
+	var endtime = parseInt(endstr[0]);
+	var numHours = endtime - starttime;
+	for (i = 0; i <= numHours; i++)
+	{
+		var row = table.insertRow(i+1);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		//cell2.setAttribute("ondrop", "drop(event)");
+		//cell2.setAttribute("ondragover","allowDrop(event)";
+		var cell3 = row.insertCell(2);
+		cell1.innerHTML = starttime+i
+	};
+	/*document.getElementById("start_timeslot").innerHTML = starttime;
+	document.getElementById("end_timeslot").innerHTML = endtime;*/
 };
