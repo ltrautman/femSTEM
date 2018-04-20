ace1d8a0eb1e48ffd87e5a8a3a0bd7a8cbadfdb7
priyappillai <pppillai@mit.edu>
2018-04-20 18:48:27 -0400

    More log in updates

--- sigma/sigmaweb/views.py
+++ sigma/sigmaweb/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'index.html', {})
